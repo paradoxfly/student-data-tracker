0b1bf4a652b618aab249f51a25a04a8ed6857b2a
paradoxfly <69152671+paradoxfly@users.noreply.github.com>
2024-02-05 22:44:07 +0100

    Added more comments

--- Classes/student.js
+++ Classes/student.js
@@ -1,3 +1,7 @@
+// Purpose: Student class to store student data and properties
+// The student has a name and properties. The properties are stored as an array of objects with label and value keys
+// The student can add properties and get the student data as an object
+// The student can also get the properties of the student
 export class Student {
   constructor(name) {
     this.name = name;
