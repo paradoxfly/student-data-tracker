7678c6bc1ccd9952b1ee517b790a811bb8d2a582
github-gke <github-gke@ezecloud-410615.iam.gserviceaccount.com>
2024-02-24 04:31:55 +0100

    Modify object class to output an object instead of arrray

--- Classes/student.js
+++ Classes/student.js
@@ -2,31 +2,44 @@
 // The student has a name and properties. The properties are stored as an array of objects with label and value keys
 // The student can add properties and get the student data as an object
 // The student can also get the properties of the student
-export class Student {
-  constructor(name) {
-    this.name = name;
+// export class Student {
+//   constructor(name) {
+//     this.name = name;
 
-    this.properties = [];
+//     this.properties = [];
 
-    // Property { label, value }[]
-  }
+//     // Property { label, value }[]
+//   }
 
-  addProperty(label, value) {
-    this.properties.push({ label, value });
-  }
+//   addProperty(label, value) {
+//     this.properties.push({ label, value });
+//   }
 
-  getStudentData() {
-    const data = { 
-      name: this.name, 
-      age: this.age,
-    };
+//   getStudentData() {
+//     const data = { 
+//       name: this.name, 
+//       age: this.age,
+//     };
     
-    for (let x of this.properties) {
-      data[x.label] = x.value;
-    }
+//     for (let x of this.properties) {
+//       data[x.label] = x.value;
+//     }
 
-    return data;
+//     return data;
+//   }
+
+
+  export class Student {
+    constructor(name) {
+      this.name = name;
+  
+      this.properties = {};
+  
   }
+  
+  addProperty(label, value) {
+    this.properties[label] = { label: label, value: value };
+}
 
   getProperties() {
     console.log(this.properties);
